eeb14ac8eb1316e3a7bd7ee50d424c5aa77e4fe4
pancake423 <dibbybucklenuggets@gmail.com>
2025-10-27 22:52:40 -0400

    chat endpoints working

--- proj2/src/server.mjs
+++ proj2/src/server.mjs
@@ -2,6 +2,11 @@ import dotenv from "dotenv";
 import { startOllama, send } from "./server/ollama-interface.mjs";
 import { COLORS, color } from "./server/terminal-helper.mjs";
 import { validateField, handleValidationErrors } from "./server/validation.mjs";
+import express from "express";
+import * as db from "./server/chat-db.mjs";
+
+// TODO: error validation (invalid id number)
+// TODO: tests, lots and lots of tests :(
 
 dotenv.config({ quiet: true });
 const ollamaStatus = await startOllama(
@@ -14,6 +19,11 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.static("src/public"));
 app.use(express.json());
+
+const id = await db.newChat();
+await db.logMessage(id, "user", "hello");
+await db.logMessage(id, "assistant", "hello ;)");
+console.log(await db.getHistory(id));
 
 /*
 making an API request:
@@ -70,6 +80,10 @@ create a new chat with the LLM.
 required headers:
 none
 
+response format (success):
+{
+  id: <int>
+}
 */
 app.post(
   "/api/chat",
@@ -77,7 +91,7 @@ app.post(
   handleValidationErrors,
   async (req, res) => {
     res.json({
-      response: "not implemented",
+      id: await db.newChat(),
     });
   },
 );
@@ -92,24 +106,40 @@ body format:
 {
   message: "your message content"
 }
+
+response format:
+{
+  response: "llm's response"
+}
 */
 app.post(
-  "/api/chat/{id}",
+  "/api/chat/:id",
   validateField("message"),
   handleValidationErrors,
   async (req, res) => {
+    // add user message to chat history
+    db.logMessage(req.params.id, "user", req.body.message);
+    // generate chat completion
+    const llm_res = await send(
+      process.env.OLLAMA_MODEL,
+      await db.getHistory(req.params.id),
+      process.env.OLLAMA_KEEP_ALIVE,
+    );
+    // log assistant response and return it
+    db.logMessage(req.params.id, "assistant", llm_res.message.content);
     res.json({
-      response: "not implemented",
+      response: llm_res.message.content,
     });
   },
 );
 
 /*
-delete an existing chat from the LLM history.
+delete an existing chat from the LLM history by id.
 */
-app.delete("/api/chat/{id}", async (req, res) => {
+app.delete("/api/chat/:id", async (req, res) => {
+  db.deleteChat(req.params.id);
   res.json({
-    response: "not implemented",
+    ok: true,
   });
 });
 
--- proj2/src/server/chat-db.mjs
+++ proj2/src/server/chat-db.mjs
@@ -1,3 +1,114 @@
 import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
+
+const INIT_SQL_SCRIPT = `
+CREATE TABLE IF NOT EXISTS chats (
+  id INTEGER PRIMARY KEY,
+  created_at TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY,
+  chat_id INTEGER NOT NULL,
+  role_id INTEGER NOT NULL,
+  content STRING NOT NULL,
+  FOREIGN KEY (chat_id)
+   REFERENCES chats (id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY (role_id)
+    REFERENCES roles (id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+INSERT INTO roles (id, name)
+VALUES
+(1, "user"),
+(2, "assistant"),
+(3, "system");
+`;
+
+async function execAsync(db, sql) {
+  return new Promise((resolve, reject) => {
+    db.exec(sql, (err) => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
+}
+
+async function getAsync(db, sql) {
+  return new Promise((resolve, reject) => {
+    db.get(sql, (err, row) => {
+      if (err) reject(err);
+      resolve(row);
+    });
+  });
+}
+
+async function allAsync(db, sql) {
+  return new Promise((resolve, reject) => {
+    db.all(sql, (err, res) => {
+      if (err) reject(err);
+      resolve(res);
+    });
+  });
+}
+
+await execAsync(db, INIT_SQL_SCRIPT);
+
+/**
+ * creates a new chat.
+ *
+ * @returns {Number} the id of the new chat.
+ */
+export async function newChat() {
+  await execAsync(
+    db,
+    `INSERT INTO chats (created_at) VALUES (datetime("now"));`,
+  );
+  return (await getAsync(db, `SELECT id FROM chats ORDER BY id DESC;`)).id;
+}
+
+/**
+ * delete a chat by id number.
+ * @param {Number} id - chat id number
+ */
+export async function deleteChat(id) {
+  await execAsync(db, `DELETE FROM chats WHERE id=${id}`);
+}
+
+/**
+ * get the complete message history for chat {id}.
+ *
+ * @param {Number} id - chat id number
+ */
+export async function getHistory(id) {
+  return await allAsync(
+    db,
+    `SELECT roles.name AS role, content FROM messages INNER JOIN roles on roles.id = messages.role_id WHERE chat_id=${id}`,
+  );
+}
+
+/**
+ * logs a message to the chat history database
+ * @param {Number} id - chat id number
+ * @param {string} role - role of the message (system, user, or assistant)
+ * @param {string} content - text contents of the message.
+ */
+export async function logMessage(id, role, content) {
+  const role_id = (
+    await getAsync(db, `SELECT id from roles WHERE name="${role}"`)
+  ).id;
+  await execAsync(
+    db,
+    `INSERT INTO messages (chat_id, role_id, content) VALUES (${id}, ${role_id}, "${content}")`,
+  );
+}
